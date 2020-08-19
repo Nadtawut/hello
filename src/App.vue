5e0ac2cba49adcc16f8cbef1bef7d464f6fb5ea8
Nadtawut <68543459+Nadtawut@users.noreply.github.com>
2020-08-19 19:27:58 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,65 @@
 <template>
+
+  <div class="row-12">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="col">
+    <img alt="me" src="./images/me1.jpg" width ="20%">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+      <div class="col"> <Counter lable="ok" @cc="sum"/> </div>
+     <CsBotton/>
+    </div>  
+    <div class="col">
+      <img alt="me" src="./images/mypro.jpg" width ="20%">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <div class="col"> <Counter lable="ok" @cc="sum"/> </div>
+       <CsBotton/>
+      </div>  
+      <div class="col">
+        <img alt="me" src="./images/myproa3.jpg" width ="20%">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <div class="col"> <Counter lable="ok" @cc="sum"/> </div>
+         <CsBotton/>
+        </div>  
+   
+    <div>
+      ชื่อ-นามสกุล <input type="text" v-model="name"/>
+      รหัส นศ. <input type="text " v-model="code">
+      GPA <input type="number" v-model="gpa" step="0.01"/>
+      <hr>
+      แสดง ชื่อ + รหัส นศ. gpa 
+    {{name}} {{code}} {{gpa}}
   </div>
+</div>
+</div>
+
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import CsBotton from './components/Csbotton'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    CsBotton,
+  },
+  data :function(){
+    return{
+    total:0,
+    name:'',
+    code:'',
+    gpa:'',
+    };
+  },
+
+  methods: {
+    
+    sum(counterValue) {
+      this.total += counterValue
+    },
+  },
 }
 </script>
 
@@ -24,5 +71,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text:center;
 }
 </style>
---
+++ src/components/Csbotton.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col">
+  <div>
+    <button @click="upFunction()" class ="btn btn-info"> <i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
+     <button @click="downFunction()" class ="btn btn-info"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+      <button @click="miniheart()" class ="btn btn-info"><i class="fa fa-heart-o" aria-hidden="true"></i></button>
+     <button @click="resetFunction()" class ="btn btn-info"><i class="fa fa-retweet" aria-hidden="true"></i></button>
+     <p v-bind:style="{fontSize: fsize + 'px'}">{{counter}}</p>
+  
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CsBotton',
+    data: function() {
+  return{
+    counter: 0,
+    fsize: 25,
+    }
+  },
+  props:{
+    label:String,
+    
+  },
+  methods : {
+  upFunction: function(){
+    this.counter += 1
+  this.$emit('cc',this.counter)
+    console.log(this.counter);
+  },
+    downFunction: function(){
+    this.counter -= 1
+    console.log(this.counter);
+    },
+     miniheart: function(){
+    this.counter += 5
+    console.log(this.counter);
+    },
+  resetFunction: function(){
+    this.counter = 0
+    console.log(this.counter);
+    },
+}
+}
+
+</script>
+
+<style>
+
+</style>
